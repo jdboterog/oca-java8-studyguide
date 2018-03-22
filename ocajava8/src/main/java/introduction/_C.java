0310fc465b706ab8d6c5e981c223a8cc1fc1c6c9
INTERGRUPO\jdboterog <jdboterog@intergrupo.com>
2018-03-22 10:50:50 -0500

    Initial commit

---
+++ ocajava8/src/main/java/introduction/_C.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class _C {
+
+	private static int $;
+	
+	public static void main(String[] args) {
+		String a_b;
+		System.out.println($);
+		//System.out.println(a_b);
+	}
+}
